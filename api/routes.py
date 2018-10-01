49e2e60135727f35e39647418ed4839735ebaed4
celestemiriams <celestemiriams95@gmail.com>
2018-10-01 23:36:45 +0300

    add new menu routes

--- api/routes.py
+++ api/routes.py
@@ -1,7 +1,7 @@
 """
 This module handles requests to urls.
 """
-
+from api.views.menu_view import MenuViews
 from api.auth.auth_view import RegisterUser, LoginUser, Logout
 
 
@@ -14,7 +14,7 @@ class Urls(object):
         """
          Generates urls on the app context
         """
-
+        menu_view = MenuViews.as_view('menu_api')
         app.add_url_rule('/api/v1/auth/signup/', view_func=RegisterUser.as_view('register_user'),
                          methods=["POST",])
         app.add_url_rule('/api/v1/auth/login/', view_func=LoginUser.as_view('login_user'),
@@ -22,5 +22,7 @@ class Urls(object):
         app.add_url_rule('/api/v1/users/logout',
                          view_func=Logout.as_view('logout_user'),
                          methods=["POST",])
+        app.add_url_rule('/api/v1/menu/', view_func=menu_view,
+                         methods=['GET', 'POST'])
 
         
