1c01c46e59e2f80650cdb11b1f3dfaeeed31266a
celestemiriams <celestemiriams95@gmail.com>
2018-10-03 18:01:48 +0300

    add more routes

--- api/routes.py
+++ api/routes.py
@@ -2,6 +2,8 @@
 This module handles requests to urls.
 """
 from api.views.menu_view import MenuViews
+from api.views.order_view import OrderViews
+from api.views.user_view import UserViews
 from api.auth.auth_view import RegisterUser, LoginUser, Logout
 
 
@@ -15,6 +17,8 @@ class Urls(object):
          Generates urls on the app context
         """
         menu_view = MenuViews.as_view('menu_api')
+        order_view = OrderViews.as_view('order_api')
+        user_view = UserViews.as_view('user_api')
         app.add_url_rule('/api/v1/auth/signup/', view_func=RegisterUser.as_view('register_user'),
                          methods=["POST",])
         app.add_url_rule('/api/v1/auth/login/', view_func=LoginUser.as_view('login_user'),
@@ -23,6 +27,18 @@ class Urls(object):
                          view_func=Logout.as_view('logout_user'),
                          methods=["POST",])
         app.add_url_rule('/api/v1/menu/', view_func=menu_view,
-                         methods=['GET', 'POST'])
+                         methods=["GET", 'POST'])
+        app.add_url_rule('/api/v1/orders/', defaults={'order_id':None},
+                         view_func=order_view, methods=["GET",])
+        app.add_url_rule('/api/v1/orders/<int:order_id>/', view_func=order_view,
+                         methods=["GET",])
+        app.add_url_rule('/api/v1/users/orders/', view_func=order_view,
+                         methods=["POST",])
+        app.add_url_rule('/api/v1/users/orders/', view_func=user_view,
+                         methods=["GET",])
+        app.add_url_rule('/api/v1/orders/<int:order_id>/', view_func=order_view,
+                         methods=["PUT",])
+        
+                
 
         
