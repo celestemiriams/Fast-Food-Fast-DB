746f9155731a48f138f2f3031bd5487e12396c01
celestemiriams <celestemiriams95@gmail.com>
2018-10-04 06:13:58 +0300

    modify put

--- api/views/order_view.py
+++ api/views/order_view.py
@@ -27,16 +27,15 @@ class OrderViews(MethodView):
             return User.decode_failure(decoded["error_message"])
 
         if User.check_login_status(decoded["user_id"]):
-            if not order_id:
-                # request_sql = """"SELECT orders.order_id,menu.item_name,menu.price,orders.quantity,orders.order_status,orders.date,users.username,users.phone_number
-                #     FROM orders
-                #     INNER JOIN menu ON menu.item_id = orders.item_id 
-                #     INNER JOIN users ON users.user_id = orders.user_id
-                #     ORDER BY orders.order_id;"""
-                request_sql = "SELECT * FROM orders"
-                sql_data = (decoded["user_id"])
-                return self.order_handler.return_all_orders(request_sql, sql_data)
-            return self.order_handler.return_single_order(order_id)
+            userId = decoded["user_id"]
+            is_admin = User.get_user_by_id(userId)
+            if decoded and is_admin:
+                if not order_id:
+                    request_sql = "SELECT * FROM orders"
+                    sql_data = (decoded["user_id"])
+                    return self.order_handler.return_all_orders(request_sql, sql_data)
+                return self.order_handler.return_single_order(order_id)
+            return jsonify({"message": "Not admin"})
             
         return jsonify({"message": "Please login"}), 401
 
@@ -65,7 +64,7 @@ class OrderViews(MethodView):
         token = request.headers.get('Authorization')
         if not token:
             return jsonify({"message": "Token is missing"}), 401
-        decoded = User.decode_token(request.headers.get('auth_token'))
+        decoded = User.decode_token(token)
         if decoded["state"] == "Failure":
             return User.decode_failure(decoded["error_message"])
         if User.check_login_status(decoded["user_id"]):
