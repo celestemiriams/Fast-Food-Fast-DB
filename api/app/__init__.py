487a015048144cb491d983fa744c5d4c7f93984b
celestemiriams <celestemiriams95@gmail.com>
2018-10-05 08:00:58 +0300

    change init contents

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -3,29 +3,22 @@ This module sets up the flask environment
 """
 
 from flask import Flask
-from api.app.handler import ErrorHandler
-from ..config.config import EnvironmentConfig, DatabaseConfig, ServerConfig
-#from ..config.config import app_config
+from api.views.handler import ErrorHandler
 from api.routes import Urls
 from api.models.database_connection import DatabaseAccess
+from instance.config import app_config
 
 
-APP = Flask(__name__)
-APP.secret_key = 'SECRET_KEY'
-APP.testing = 'TESTING'
-APP.debug = 'DEBUG'
-APP.env = 'ENVIRONMENT'
-APP.errorhandler(404)(ErrorHandler.url_not_found)
+def create_app(config_name):
+    APP = Flask(__name__, instance_relative_config=True)
+    APP.config.from_object(app_config[config_name])
+    APP.config.from_pyfile('config.py')
+    APP.secret_key = 'SECRET_KEY'
+    APP.testing = 'TESTING'
+    APP.debug = 'DEBUG'
+    APP.env = 'ENVIRONMENT'
+    APP.errorhandler(404)(ErrorHandler.url_not_found)
+    Urls.generate_url(APP)
+    DatabaseAccess.create_tables()
 
-Urls.generate_url(APP)
-DatabaseAccess.create_tables()
-
-# def create_app(config_name):
-#     APP = Flask(__name__)
-#     APP.config.from_object(app_config[config_name])
-#     APP.config.from_pyfile('config.py')
-#     APP.errorhandler(404)(ErrorHandler.url_not_found)
-#     Urls.generate_url(APP)
-#     DatabaseAccess.create_tables()
-
-#     return APP
+    return APP
