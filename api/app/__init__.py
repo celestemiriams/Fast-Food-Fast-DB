c72518c05e67317c0d6bc388cdc616b21f7fb573
celestemiriams <celestemiriams95@gmail.com>
2018-10-05 04:26:26 +0300

    modify run

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -5,6 +5,7 @@ This module sets up the flask environment
 from flask import Flask
 from api.app.handler import ErrorHandler
 from ..config.config import EnvironmentConfig, DatabaseConfig, ServerConfig
+#from ..config.config import app_config
 from api.routes import Urls
 from api.models.database_connection import DatabaseAccess
 
@@ -18,3 +19,13 @@ APP.errorhandler(404)(ErrorHandler.url_not_found)
 
 Urls.generate_url(APP)
 DatabaseAccess.create_tables()
+
+# def create_app(config_name):
+#     APP = Flask(__name__)
+#     APP.config.from_object(app_config[config_name])
+#     APP.config.from_pyfile('config.py')
+#     APP.errorhandler(404)(ErrorHandler.url_not_found)
+#     Urls.generate_url(APP)
+#     DatabaseAccess.create_tables()
+
+#     return APP
