1fdbf7518e7c2dc5a10fe8d04b874c93ba764baf
celestemiriams <celestemiriams95@gmail.com>
2018-10-05 04:26:43 +0300

    modify run file

--- run.py
+++ run.py
@@ -2,7 +2,13 @@
 This module runs the application
 """
 
+import os
 
+#from api.app import create_app
 from api.app import APP
+
+# config_name = os.getenv('APP_SETTINGS') # config_name = "development"
+# app = create_app(config_name)
+
 if __name__ == '__main__':
     APP.run()
