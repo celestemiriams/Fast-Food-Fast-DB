de83e22f96c528ee73391b86c5b53c6861b6c858
celestemiriams <celestemiriams95@gmail.com>
2018-10-05 21:16:50 +0300

    change init file

--- api/app/__init__.py
+++ api/app/__init__.py
@@ -1,24 +1,20 @@
 """
-This module sets up the flask environment
+This module sets up the flask emvironment
 """
 
 from flask import Flask
 from api.views.handler import ErrorHandler
+from ..instance.config import EnvironmentConfig, DatabaseConfig, ServerConfig
 from api.routes import Urls
 from api.models.database_connection import DatabaseAccess
-from instance.config import app_config
 
 
-def create_app(config_name):
-    APP = Flask(__name__, instance_relative_config=True)
-    APP.config.from_object(app_config[config_name])
-    APP.config.from_pyfile('config.py')
-    APP.secret_key = 'SECRET_KEY'
-    APP.testing = 'TESTING'
-    APP.debug = 'DEBUG'
-    APP.env = 'ENVIRONMENT'
-    APP.errorhandler(404)(ErrorHandler.url_not_found)
-    Urls.generate_url(APP)
-    DatabaseAccess.create_tables()
+APP = Flask(__name__)
+APP.secret_key = 'SECRET_KEY'
+APP.testing = 'TESTING'
+APP.debug = 'DEBUG'
+APP.env = 'ENVIRONMENT'
+APP.errorhandler(404)(ErrorHandler.url_not_found)
 
-    return APP
+Urls.generate_url(APP)
+DatabaseAccess.create_tables()
